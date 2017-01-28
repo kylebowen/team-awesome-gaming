b8c19f89dde183306b1788aa56072eb2961c848f
Kyle Bowen <kyle.m.bowen@gmail.com>
2017-01-27 22:34:43 -0800

    Add User, styles, and placeholder content on static pages

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_url
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 3
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://www.latimes.com/"
+    assert_select "a[href=?]", "https://github.com/kylebowen"
+  end
+end
