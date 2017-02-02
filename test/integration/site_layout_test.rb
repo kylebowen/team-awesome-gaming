737d33b49df9f272c4b2c47ca0c06644b6319c86
Kyle Bowen <kyle.m.bowen@gmail.com>
2017-02-01 21:24:41 -0800

    Add tests for logged-in site layout links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,17 +2,38 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "logged-out layout links" do
     get root_url
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", "http://www.latimes.com/"
     assert_select "a[href=?]", "https://github.com/kylebowen"
   end
 
-
+  test "logged-in layout links" do
+    post login_path params: { session: { email: @user.email,
+                                         password: 'password' } }
+    assert is_logged_in?
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 3
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://www.latimes.com/"
+    assert_select "a[href=?]", "https://github.com/kylebowen"
+  end
 end
